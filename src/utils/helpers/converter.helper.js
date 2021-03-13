c1e2dc82a2303436cbb168de4be93e3fd46db87c
Tran Ba Hoa <tran.ba.hoa@sun-asterisk.com>
2021-03-13 12:08:32 +0700

    format text

--- src/utils/helpers/converter.helper.js
+++ src/utils/helpers/converter.helper.js
@@ -1,44 +1,57 @@
+function formatTextSpaces(text, nextText, prevText, isStart, isEnd) {
+  const noBeforeSpaceCharacters = ['.', ',', ':', ')', '}', ']', '"', "'", ';', '“'];
+  const noAfterSpaceCharacters = ['(', '[', '{', '"', "'"];
+  let after = ' ';
+  if (isEnd || noBeforeSpaceCharacters.includes(nextText) || noAfterSpaceCharacters.includes(text)) after = '';
+
+  return text + after;
+}
+
 function convertCONLLToJSONL(data, lineSeparator) {
   const newData = [];
   let jsonlLine = {
     text: '',
     labels: [],
   };
-  let texts = [];
   let currentType = 'O';
   let offsetStart = 0;
 
   // Loops though data
   data.forEach((conllLine, index) => {
     const isEndOfData = index === data.length - 1;
+    const isGoodCollData = conllLine && conllLine.text && conllLine.label;
 
-    if (conllLine && conllLine.text) {
+    if (isGoodCollData) {
       const splitIndex = conllLine.label.indexOf('-');
       const type = conllLine.label === 'O' ? 'O' : conllLine.label.substring(splitIndex + 1);
 
       // When type changed/end of data
-      if (currentType !== type || isEndOfData) {
+      if (currentType !== type) {
         if (currentType !== 'O') jsonlLine.labels.push([offsetStart, jsonlLine.text.length, currentType]);
 
         currentType = type;
         offsetStart = jsonlLine.text.length;
-      } else if (!currentType.length) {
-        currentType = type;
-        offsetStart = jsonlLine.text.length;
       }
 
-      texts.push(conllLine.text);
-      jsonlLine.text = texts.join(' ');
+      jsonlLine.text += formatTextSpaces(
+        conllLine.text,
+        data[index + 1] ? data[index + 1].text : '',
+        data[index - 1] ? data[index - 1].text : '',
+        index === 0,
+        index === data.length - 1
+      );
+
+      // When end of data but the last one has concept
+      if (isEndOfData && currentType !== 'O') jsonlLine.labels.push([offsetStart, jsonlLine.text.length, currentType]);
     }
 
     // When reach line seperator/end of sentence/end of data
-    if (!conllLine || !conllLine.text || lineSeparator(conllLine, data, index) || isEndOfData) {
+    if (!isGoodCollData || lineSeparator(conllLine, data, index) || isEndOfData) {
       if (jsonlLine.text.length) newData.push(jsonlLine);
       jsonlLine = {
         text: '',
         labels: [],
       };
-      texts = [];
       currentType = 'O';
     }
   });
