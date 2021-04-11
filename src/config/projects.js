2061c20f36131b44f1a6bb2d388a79ccb2d3978a
Tran Ba Hoa <tran.ba.hoa@sun-asterisk.com>
2021-04-12 06:01:51 +0700

    Doing with project types

--- src/config/projects.js
+++ src/config/projects.js
@@ -4,6 +4,13 @@ const projectRoles = {
   PROJECT_ADMIN: 'Project Admin',
 };
 
+const projectTypes = {
+  SEQ_LABEL: 'Sequence Labeling',
+  DOC_CLASS: 'Document Classification',
+  SEQ_TRANS: 'Sequence Translation',
+};
+
 module.exports = {
   projectRoles,
+  projectTypes,
 };
