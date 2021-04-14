cf3ecea37af7b914258f331852c6e2c4ed298b70
Tran Ba Hoa <tran.ba.hoa@sun-asterisk.com>
2021-04-15 05:08:49 +0700

    Doing with label set

--- src/controllers/label-set.controller.js
+++ src/controllers/label-set.controller.js
@@ -9,25 +9,25 @@ const getAllLabelSets = catchAsync(async (req, res) => {
 });
 
 const getLabelSet = catchAsync(async (req, res) => {
-  const label = await labelSetService.getLabelSetById(req.params.labelId);
-  if (!label) {
+  const labelSet = await labelSetService.getLabelSetById(req.params.labelSetId);
+  if (!labelSet) {
     throw new ApiError(httpStatus.NOT_FOUND, 'Label set not found');
   }
-  res.send(label);
+  res.send(labelSet);
 });
 
 const createLabelSet = catchAsync(async (req, res) => {
-  const project = await labelSetService.createLabelSet(req.user, req.body);
-  res.status(httpStatus.CREATED).send(project);
+  const labelSet = await labelSetService.createLabelSet(req.user, req.body);
+  res.status(httpStatus.CREATED).send(labelSet);
 });
 
 const updateLabelSet = catchAsync(async (req, res) => {
-  const label = await labelSetService.updateLabelSetById(req.params.labelId, req.body);
-  res.send(label);
+  const labelSet = await labelSetService.updateLabelSetById(req.params.labelSetId, req.body);
+  res.send(labelSet);
 });
 
 const deleteLabelSet = catchAsync(async (req, res) => {
-  await labelSetService.deleteLabelSetById(req.params.labelId);
+  await labelSetService.deleteLabelSetById(req.params.labelSetId);
   res.status(httpStatus.NO_CONTENT).send();
 });
 
