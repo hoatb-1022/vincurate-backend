4718507cd0407fc284fb4f56e6d7614bb0c2076d
Tran Ba Hoa <tran.ba.hoa@sun-asterisk.com>
2021-06-03 20:01:16 +0700

    Fix name of ML

--- ML/run.sh
+++ ML/run.sh
@@ -1,2 +1,1 @@
-#java -Xmx2g -jar VnCoreNLP-1.1.1.jar -fin input.txt -fout output.txt
-vncorenlp -Xmx2g VnCoreNLP-1.1.1.jar -p 9000 -a "wseg,pos,ner,parse"
+vncorenlp -Xmx2g ML.jar -p 9000 -a "wseg,pos,ner,parse"
